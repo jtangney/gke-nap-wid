c4ac1b78e6a2e66f31f5ed2022f7aef7ea0a2b48
jtangney <jeremy.tangney@gmail.com>
2020-05-05 10:30:37 +0100

    refactored and extended job Kustomizations

--- pi/pi.go
+++ pi/pi.go
@@ -4,32 +4,37 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"
 	"math"
+	"strconv"
 	"time"
 
 	"cloud.google.com/go/storage"
+	"k8s.io/klog"
 )
 
 var (
-	workers = flag.Int("workerCount", 500000, "Number of go routines used to calculate pi")
-	bucket  = flag.String("bucket", "", "Write Pi result to this GCS bucket")
+	workers  = flag.Int("workers", 500000, "Number of go routines used to calculate pi")
+	calcTime = flag.Duration("calcTime", 10*time.Second, "Calculate pi for this length of time (seconds)")
+	bucket   = flag.String("bucket", "", "Write Pi result to this GCS bucket")
 )
 
 func main() {
 	flag.Parse()
-	val := pi(*workers)
-	log.Printf("Calculated Pi with %d goroutines: %f\n", *workers, val)
+	val := pi2(*calcTime)
+	// val := pi(*workers)
+	strval := strconv.FormatFloat(val, 'f', -1, 64)
+	// log.Printf("Calculated Pi with %d goroutines: %s\n", *workers, strval)
+	klog.Infof("Calculated Pi for %v: %s\n", *calcTime, strval)
 	if *bucket != "" {
-		writeToGcs(*bucket, val)
+		writeToGcs(*bucket, strval)
 	}
 }
 
-func writeToGcs(bucketName string, val float64) {
+func writeToGcs(bucketName string, val string) {
 	ctx := context.Background()
 	client, err := storage.NewClient(ctx)
 	if err != nil {
-		log.Fatalf("Failed to create GCS client: %v", err)
+		klog.Exitf("Failed to create GCS client: %v", err)
 	}
 
 	bucket := client.Bucket(bucketName)
@@ -37,13 +42,28 @@ func writeToGcs(bucketName string, val float64) {
 	obj := bucket.Object(filename)
 
 	w := obj.NewWriter(ctx)
-	if _, err := fmt.Fprintf(w, "%f", val); err != nil {
-		log.Fatalf("Failed to write GCS file: %v", err)
+	if _, err := fmt.Fprintf(w, "%s", val); err != nil {
+		klog.Exitf("Failed to write GCS file: %v", err)
 	}
 	if err := w.Close(); err != nil {
-		log.Printf("Error closing writer: %v", err)
+		klog.Exitf("Failed to write GCS file: %v", err)
 	}
-	log.Printf("Wrote gs://%s/%s", bucketName, filename)
+	klog.Infof("Wrote gs://%s/%s", bucketName, filename)
+}
+
+// approximate pi using the Leibniz formula for specified duration
+func pi2(calcTime time.Duration) float64 {
+	f := 0.0
+	k := 0.0
+	for timeout := time.After(calcTime); ; {
+		select {
+		case <-timeout:
+			return f
+		default:
+			f += 4 * math.Pow(-1, k) / (2*k + 1)
+			k++
+		}
+	}
 }
 
 // pi launches n goroutines to compute an
